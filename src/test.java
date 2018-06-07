4e4ad000bcb438b67087175165e0f5c438aa8b1d
playwolf719 <playwolf719@163.com>
2018-06-07 08:02:23 +0800

    first init

---
+++ src/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by playwolf on 3/31/2018.
+ */
+public class test {
+    public static void main(String[] args) throws Exception {
+        System.out.println("尼玛特殊");
+    }
+}
---
+++ src/test1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by playwolf on 3/31/2018.
+ */
+public class test1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("尼玛1");
+    }
+}
